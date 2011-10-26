ec15d9cd39bbeea247b96ee33da003aaccfbf087
Marc van der Sluys <git@vandersluys.nl>
2011-10-26 17:52:54 +0200

    Added CMake files

---
+++ CMake/FileList.cmake
@@ -0,0 +1,11 @@
+set(SPINspiral_src_files
+  src/SPINspiral_3rdparty.c  
+  src/SPINspiral_data.c  
+  src/SPINspiral_lal.c  
+  src/SPINspiral_main.c  
+  src/SPINspiral_mcmc.c  
+  src/SPINspiral_parameters.c  
+  src/SPINspiral_routines.c  
+  src/SPINspiral_signal.c  
+  src/SPINspiral_templates.c
+  )
